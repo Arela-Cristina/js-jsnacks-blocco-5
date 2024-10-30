c2cc319e907ce5d1767e00f8aff48d61927a036b
Arela-Cristina <instagrandine@gmail.com>
2024-10-30 14:49:42 +0100

    init

---
+++ ex-js-array-methods-main/snack-1/script.js
@@ -0,0 +1,9 @@
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+// Stampa in console tutti i nomi
+// Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//prendo ogni elemnto del array forEach 
+
+//arrow function implicita
+names.forEach((name) => console.log('elemento:', name)); //stringa 
