c64df1fdeec8dc7939d27bde891a7489a77019ff
Arela-Cristina <instagrandine@gmail.com>
2024-10-30 17:02:06 +0100

    metodo find

--- ex-js-array-methods-main/snack-7/script.js
+++ ex-js-array-methods-main/snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+//metodo Find e destructuring, recuperiamo un dato/oggetto che ha il id 2
+const found = students.find(({id}) => id === 2);
+
+console.log(found)
